5a637e555b8498bd8ebd0c3ab20c00878588a04c
davidjr <quachhuycm@gmail.com>
2024-10-16 16:18:54 +0700

    first hello world app

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,8 @@
+class ApplicationController < ActionController::Base
+  def hello
+    render html: "Hello, world !"
+  end
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  allow_browser versions: :modern
+  
+end
