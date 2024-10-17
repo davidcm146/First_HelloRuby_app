2d79f1acd9c8495c141e9d92a4bce4a5946f6f4f
davidjr <quachhuycm@gmail.com>
2024-10-17 09:42:41 +0700

    change hello to goodbye

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   def hello
-    render html: "Hello, world !"
+    render html: "Goodbye, world !"
   end
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
